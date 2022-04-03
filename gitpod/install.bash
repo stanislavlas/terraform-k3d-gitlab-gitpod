233c7cdd08788d7559a9589b1d38f6d5f34ebdc2
Administrator <admin@example.com>
2022-04-03 11:51:07 +0200

    update

--- gitpod/install.bash
+++ gitpod/install.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function check_deployments() {
-    deployments="$(kubectl get deployments -n $1 -o custom-columns=":metadata.name")"
+    deployments="$(k3s kubectl get deployments -n $1 -o custom-columns=":metadata.name")"
     for deployment in $deployments; do
-        kubectl rollout status deployment -n $1 $deployment
+        k3s kubectl rollout status deployment -n $1 $deployment
     done   
 }
 
@@ -12,24 +12,24 @@ mount --make-shared /proc
 mount --make-shared /var/gitpod/workspaces
 
 CERTS=$(pwd)/certs
-kubectl create secret tls https-certificates \
+k3s kubectl create secret tls https-certificates \
     --cert="$CERTS/fullchain.pem" \
     --key="$CERTS/privkey.pem"
 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.yaml
+k3s kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.yaml
 sed "s+<domain>+${DOMAIN}+g" "gitpod/gitpod.config.yaml" > gitpod.config.yaml
 
 if [ "$ENABLE_AUTH_PROVIDER" = "true" ]
 then 
     cat gitpod/gitpod.authProvider.config.yaml >> gitpod.config.yaml
     PROVIDER=$(sed "s+<clientId>+${CLIENT_ID}+g; s+<clientSecret>+${CLIENT_SECRET}+g; s+<domain>+${DOMAIN}+" "gitpod/gitlab-oauth.yaml")
-    kubectl create secret generic --from-literal=provider="$PROVIDER" gitlab-oauth
+    k3s kubectl create secret generic --from-literal=provider="$PROVIDER" gitlab-oauth
 fi
 
 check_deployments "cert-manager"
 gitpod-installer render --config gitpod.config.yaml > gitpod.yaml
 
-kubectl apply -f gitpod.yaml
+k3s kubectl apply -f gitpod.yaml --validate=false
 
 check_deployments "default"
 
